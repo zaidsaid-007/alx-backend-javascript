46132df004b11fbbd7f341ce2e522ec37f9b2483
zaidsaid-007 <zaidsaid007@gmail.com>
2024-09-18 22:37:10 +0300

    complex http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,17 +4,32 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
+
+  if (req.url === '/') {
+    // Handle root path "/"
+    res.write('Hello Holberton School!');
+    res.end(); // Ensure response ends properly here
+  } else if (req.url === '/students') {
+    // Handle "/students" path
     res.write('This is the list of our students\n');
+    
+    // Process the students list asynchronously
     try {
       const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
+      res.end(`${data.join('\n')}`); // Append the student list to the response and end it
     } catch (error) {
-      res.end(error.message);
+      res.end(error.message); // If there's an error, display the error message
     }
+  } else {
+    // If a different URL is accessed, return a 404 status code
+    res.statusCode = 404;
+    res.end('Not Found');
   }
-  res.end();
 });
-app.listen(1245);
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
 module.exports = app;
+
